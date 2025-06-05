bab67da0d4f5703c4998e1bacc5801f5f41092f8
Serhii Veretennikov <gridis72@gmail.com>
2025-06-05 17:55:30 +0200

    feat: created test file

--- ex00/testBureaucrat.cpp
+++ ex00/testBureaucrat.cpp
@@ -6,7 +6,17 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 14:32:32 by svereten          #+#    #+#             */
-/*   Updated: 2025/05/29 14:32:33 by svereten         ###   ########.fr       */
+/*   Updated: 2025/06/05 17:09:34 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "Bureaucrat.hpp"
+#include <gtest/gtest.h>
+#include <exception>
 
+TEST(Bureaucrat, constructors) {
+	Bureaucrat a;
+	Bureaucrat b("Johnny", 150);
+
+	EXPECT_THROW(Bureaucrat tooLow("Low", 151), Bureaucrat::GradeTooLowException);
+	EXPECT_THROW(Bureaucrat tooHigh("High", 0), Bureaucrat::GradeTooLowException);
+}
